55e140a268fcca4cdb3fe94bfbd0b87e85f329dc
Baileigh Dabdoub <bdabdo2@lsu.edu>
2019-09-18 11:46:46 -0500

    last update

--- testCoding/GitHubHW/Part3/githubhwloop.js
+++ testCoding/GitHubHW/Part3/githubhwloop.js
@@ -6,6 +6,13 @@ function setup(){
 
 
  function draw(){
+     background(0);
+    for(var x=0;x<=width;x+=50){
+        for(var y=0;y<=height;y+=50){
+            fill(random(255),0,random(255));
+            ellipse(x,y,30,30);
+        }
+    }
  }
 
   
