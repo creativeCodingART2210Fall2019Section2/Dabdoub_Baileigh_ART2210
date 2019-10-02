e59e7fcc1ccf8948d428cc439693355809bf0528
Baileigh Dabdoub <bdabdo2@lsu.edu>
2019-10-02 14:51:49 -0500

    update

--- PictureGithubTest/picturegithub.js
+++ PictureGithubTest/picturegithub.js
@@ -1,8 +1,13 @@
+var img;
+
+function preload(){
+    img=loadImage("https://github.com/BaileighD/Dabdoub_Baileigh_ART2210/raw/master/PictureGithubTest/tiger.jpg");
+}
 function setup(){
     createCanvas(windowWidth,windowHeight);
 }
  function draw(){
-
+Image(img,windowWidth/6,windowHeight/6, windowWidth/2, windowHeight/2);
 
  }
 
